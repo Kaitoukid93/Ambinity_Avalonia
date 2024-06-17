1a1d1d36a7f272182760cf5a6f26a6f4d20f3ec3
Kaitoukid93 <saorihara93@gmail.com>
2024-06-17 13:50:51 +0700

    Create new project and testing new IDE function

---
+++ Ambinity/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.ObjectModel;
+namespace Ambinity.ViewModels
+{
+    public partial class MainWindowViewModel : ViewModelBase
+    {
+        public MainWindowViewModel()
+        {
+                GetAvailableComPorts();
+        }
+        public string Greeting => "Welcome to Avalonia!";
+        public ObservableCollection<string> AvailableComPorts { get; set; }
+        private void GetAvailableComPorts()
+        {
+            AvailableComPorts = new ObservableCollection<string>();
+
+            foreach (var port in System.IO.Ports.SerialPort.GetPortNames())
+            {
+                AvailableComPorts.Add(port);
+            }
+           
+            
+        }
+       
+    }
+}
---
+++ Ambinity/Views/MainWindow.axaml.cs
@@ -0,0 +1,18 @@
+using System.Diagnostics;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.PanAndZoom;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+
+namespace Ambinity.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
